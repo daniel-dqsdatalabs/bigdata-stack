11bbeed9c70b358a3464669c2b2119241a48c2ec
Daniel Q <daniel@dqsdatalabs.com>
2024-10-04 10:06:56 -0300

    add northwind database

--- docker/minio/minio-entrypoint.sh
+++ docker/minio/minio-entrypoint.sh
@@ -3,6 +3,9 @@
 sleep 5
 /usr/bin/mc config host add local http://minio:9000 minio minio123
 /usr/bin/mc mb local/warehouse
+/usr/bin/mc mb local/warehouse/bronze
+/usr/bin/mc mb local/warehouse/silver
+/usr/bin/mc mb local/warehouse/gold
 /usr/bin/mc mb local/flink
 /usr/bin/mc mb local/flink/flink-savepoints
 /usr/bin/mc mb local/flink/flink-checkpoints
